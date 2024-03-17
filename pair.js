368a95fae2843771bcab1e7ca4d43205314179c6
darkshan1234 <149415238+darkshan1234@users.noreply.github.com>
2024-03-17 11:11:01 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,85 +1,69 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
-const express = require('express');
-const fs = require('fs');
-let router = express.Router()
-const pino = require("pino");
+require('./send.js')
+require('@adiwajshing/keyed-db')
 const {
-    default: Maher_Zubair,
+    default: sockConnect,
     useMultiFileAuthState,
-    delay,
-    makeCacheableSignalKeyStore,
-    Browsers
-} = require("maher-zubair-baileys");
+    fetchLatestBaileysVersion,
+    jidNormalizedUser
+} = require('@adiwajshing/baileys')
+const {
+    upload
+} = require('./mega')
+var fs = require('fs')
+const pino = require('pino')
+var auth_path = './auth_info_baileys/'
+async function start() {
+    var {
+        version
+    } = await fetchLatestBaileysVersion()
 
-function removeFile(FilePath){
-    if(!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true })
- };
-router.get('/', async (req, res) => {
-    const id = makeid();
-    let num = req.query.number;
-        async function SIGMA_MD_PAIR_CODE() {
-        const {
-            state,
-            saveCreds
-        } = await useMultiFileAuthState('./temp/'+id)
-     try {
-            let Pair_Code_By_Maher_Zubair = Maher_Zubair({
-                auth: {
-                    creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
-                },
-                printQRInTerminal: false,
-                logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Chrome (Linux)", "", ""]
-             });
-             if(!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
-                await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Maher_Zubair.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
-            Pair_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
-                const {
-                    connection,
-                    lastDisconnect
-                } = s;
-                if (connection == "open") {
-                await delay(5000);
-                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                await delay(800);
-               let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'DARKSHAN;;;' + b64data });
+    try {
+const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
+  const sock = sockConnect({
+    printQRInTerminal: true,
+    version: [2,2323,4],
+    browser: ['Zero-Two-MD', 'Web', 'v2'],
+    auth: state,
+    version
+  });
+        sock.ev.on('creds.update', saveCreds)
 
-               let SIGMA_MD_TEXT = `
-*_Pair Code By Maher Zubair_*
-*_Made With 🤍_*
+        sock.ev.on('connection.update', async (update) => {
+            const {
+                connection
+            } = update
+            if (connection === 'close') {
+                start()
+            }
+            if (update.qr) {
+                qr_codde = update.qr
+                         const QRLogo = require('qr-with-logo');
+ const data = JSON.stringify(qr_codde)
+ await QRLogo.generateQRWithLogo(data.replace(/"/g,''), "zen.png", {}, "Base64", "qrlogo.png", async function(b64) {
+             qr_code = b64
+   //console.log(b64)
+ });
+            }
 
-_Don't Forget To Give Star To My Repo_`
- await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
- 
-
-        await delay(100);
-        await Pair_Code_By_Maher_Zubair.ws.close();
-        return await removeFile('./temp/'+id);
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                    await delay(10000);
-                    SIGMA_MD_PAIR_CODE();
-                }
-            });
-        } catch (err) {
-            console.log("service restated");
-            await removeFile('./temp/'+id);
-         if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
-         }
-        }
+            if (connection === 'open') {
+                qr_code = ''
+                const user_jid = jidNormalizedUser(sock.user.id);
+                const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${user_jid}.json`);
+                const string_session = 'IZUMI=' + mega_url.replace('https://mega.nz/file/', '')
+                await sock.sendMessage(user_jid, {
+                    text: string_session
+                });
+                
+                fs.rmSync(auth_path, {
+                    recursive: true,
+                    force: true
+                })
+                start()
+            }
+        })
+    } catch {
+        start()
     }
-    return await SIGMA_MD_PAIR_CODE()
-});
-module.exports = router
+}
+
+start()
